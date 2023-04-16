63f4a4ae5c50a475c1c746bd4cf4149c5922beb3
Mara44sum41 <you@example.commarbord1@gmail.com>
2023-04-16 22:54:09 +0200

    Add project files.

---
+++ RWStask/Page Objects/Class1.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+
+public class WikipediaPage
+{
+    private IWebDriver driver;
+
+    public WikipediaPage(IWebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    public void NavigateTo(string url)
+    {
+        driver.Navigate().GoToUrl(url);
+    }
+
+    public void ClickRandomLink()
+    {
+        var links = driver.FindElements(By.CssSelector("a[href^='/wiki/']"));
+        var random = new Random();
+        var randomLink = links[random.Next(links.Count)];
+        randomLink.Click();
+    }
+
+    public void ClickPhilosophyLink()
+    {
+        var philosophyLinks = driver.FindElements(By.XPath("//a[@href='/wiki/Philosophy']"));
+        if (philosophyLinks.Count > 0)
+        {
+            philosophyLinks.First().Click();
+        }
+    }
+
+    public void GoBack()
+    {
+        driver.Navigate().Back();
+    }
+}
---
+++ RWStask/Program.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V110.WebAuthn;
+
+class Program
+{
+    static void Main()
+    {
+        var driver = new ChromeDriver();
+        var wikipediaPage = new WikipediaPage(driver);
+
+        wikipediaPage.NavigateTo("https://en.wikipedia.org/…age");
+
+        var reached = false;
+        var redirects = 0;
+        while (!reached)
+        {
+            try
+            {
+                wikipediaPage.ClickRandomLink();
+            }
+            catch (ElementNotInteractableException)
+            {
+                wikipediaPage.GoBack();
+                continue;
+            }
+
+            redirects++;
+            wikipediaPage.ClickPhilosophyLink();
+            if (driver.Url.EndsWith("/Philosophy"))
+            {
+                reached = true;
+                Console.Clear();
+                Console.WriteLine($"Philosophy found after {redirects} redirects.");
+                Console.ReadKey();
+            }
+        }
+        driver.Quit();
+    }
+}
